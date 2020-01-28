7c3dbc4a04e717cd862e17dcb0bf4eb36d0e438f
Elysia Gabe <elysiagabe@gmail.com>
2020-01-28 13:03:02 -0800

    Answers challenge counterMaker

--- index.js
+++ index.js
@@ -264,9 +264,9 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = -1;
+  return function counter() {
+    return ++count
   }
   // BROKEN CODE ENDS
 }
