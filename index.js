c72acd5c84739e8ae87996e88b2041cdecda4c8d
Elysia Gabe <elysiagabe@gmail.com>
2020-01-28 13:48:03 -0800

    Answers stretch task processDuplicateFree

--- index.js
+++ index.js
@@ -159,8 +159,9 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  const deDuped = Array.from(new Set(list));
+  return callback(deDuped);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
